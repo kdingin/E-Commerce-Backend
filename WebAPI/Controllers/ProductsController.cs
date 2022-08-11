47cceab7fe9549f1acddf32431a0fbb608a6719f
fatih Küçükdingin <fatihkdingin@gmail.com>
2022-08-11 04:36:34 +0300

    11.08.2022

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public List<Product> Get()
+        {
+            
+            var result= _productService.GetAll();
+            return result.Data;
+        }
+    }
+}
