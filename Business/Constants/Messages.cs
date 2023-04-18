7d30015722938efb41060b6d3df396640f2611f6
fatih Küçükdingin <68949521+kdingin@users.noreply.github.com>
2023-04-18 05:38:50 +0300

    .

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -13,6 +14,13 @@ namespace Business.Constants
         public static string ProductsListed="Ürünler Listelendi";
         public static string ProductCountOfCategoryError="Bir kategoride en fazla 10 ürün olabilir.";
         public static string ProductNameAlreadyExists="Bu isimde bir ürün zaten kullanılmaktadır.";
-        internal static string CategoryLimitExceded="Kategori limiti aşılmıştır.";
+        public static string CategoryLimitExceded="Kategori limiti aşılmıştır.";
+        public static string AuthorizationDenied = "Yetkilendirme yok.";
+        public static string UserRegistered="kayıt oldu";
+        public static string UserNotFound="Kullanıcı Bulunamadı";
+        public static string PasswordError="Parola hatalı";
+        public static string SuccessfulLogin="Başarılı giriş";
+        public static string UserAlreadyExists="Kullanıcı mevcut";
+        public static string AccessTokenCreated="Token oluşturuldu";
     }
 }
